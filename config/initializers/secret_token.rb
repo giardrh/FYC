c99fff0ceff5e7189cd4eec53949f5a23e315da4
Ryley Giard <giardrh@gmail.com>
2013-03-10 16:35:01 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FycApp::Application.config.secret_token = '2d07e54bcdc544a1c5a3937a0f018849007ec3e9ded3fdf657cb7e5fa165a103eb62413ce5f89797042c8e77ee5ce58a3185fb32d6218a2a98f78d61aa90189f'
