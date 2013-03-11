7a05e4337cbef1fc0f20ba2b8ab0f26954039c55
Ryley Giard <giardrh@gmail.com>
2013-03-11 14:31:43 -0600

    Removed index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 FycApp::Application.routes.draw do
+  get "home/index"
+
   get "static_pages/home"
 
   get "static_pages/help"
@@ -62,7 +64,7 @@ FycApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
