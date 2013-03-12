7002aef31b6e45d1b434bb0afa79db9d560b5da9
Ryley Giard <giardrh@gmail.com>
2013-03-12 17:06:15 -0600

    Added layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,10 @@
 FycApp::Application.routes.draw do
-  get "home/index"
-
-  get "static_pages/home"
-
-  get "static_pages/help"
+  root :to => 'static_pages#home'
   
-  get "static_pages/about"
-  
-  get "static_pages/contact"
 
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
  
 
 
@@ -68,7 +64,7 @@ FycApp::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-root :to => 'home#index'
+
 
   # See how all your routes lay out with "rake routes"
 
